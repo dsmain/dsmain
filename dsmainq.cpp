997e4c3816e659a2a45a7d84c9ce07a89299b8eb
dsmain <153432639+dsmain@users.noreply.github.com>
2023-12-10 21:16:38 +0530

    dsmainq.cpp

--- dsmainq.cpp
+++ dsmainq.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  
 Q1_Single_Linked_List
 Q2_Double_Linked_List
 Q3_Circular_Linked_List
@@ -5,3 +11,8 @@ Q4_Stack_Prefix_postpix
 Q5_Queue_ADT
 Q6_Binary_Tree
 Q7_AVL_Trees_Insert_Search
+
+
+  
+  return 0;
+}
